4ada7325c36d6e3fde7f480c882dfa09ce225051
Markus Machel <machelmarkus@gmail.com>
2022-05-22 12:13:47 -0300

    aula 4 validação padão

--- src/app/home/login/login.component.ts
+++ src/app/home/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AutenticacaoService } from 'src/app/autenticacao/autenticacao.service';
 
 @Component({
@@ -11,14 +12,14 @@ export class LoginComponent implements OnInit {
   usuario = '';
   senha = '';
 
-  constructor(private authService: AutenticacaoService) { }
+  constructor(private authService: AutenticacaoService, private  router: Router) { }
 
   ngOnInit(): void {
   }
 
   login() {
     this.authService.autenticar(this.usuario, this.senha).subscribe({
-      next: () => console.log('Autenticado com sucesso!'),
+      next: () => this.router.navigate(['animais']),
       error: (error) => alert('Usuário ou senha inválida!')
     });
   }
